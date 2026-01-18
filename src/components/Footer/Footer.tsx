ca91d6a8f0852ba1c2f55f9a1842c028e2ecda5c
Mathis Engels <mathisengels@yahoo.fr>
2026-01-18 16:49:40 +0100

    revert footer changes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import { Coffee, Github } from "lucide-react";
 
 export default function Footer() {
   return (
-    <footer className="flex w-fit flex-col items-center gap-pag-spacing-50 text-pag-xs text-pag-text-muted">
+    <footer className="flex w-fit flex-col items-center gap-pag-spacing-50 text-pag-xs">
       <a
         href="https://github.com/LesCop1/PickAGame-front"
         target="_blank"
@@ -10,11 +10,11 @@ export default function Footer() {
         className="flex items-center gap-pag-spacing-50"
       >
         <Github strokeWidth={1} size={16} className="text-pag-icon-primary-hover" />
-        <span>Github</span>
+        <span className="text-pag-text-muted">Github</span>
       </a>
       <a href="#" target="_blank" rel="noopener noreferrer" className="flex items-center gap-pag-spacing-50">
         <Coffee strokeWidth={1} size={16} className="text-pag-icon-primary-hover" />
-        <span>Buy us a coffee</span>
+        <span className="text-pag-text-muted">Buy us a coffee</span>
       </a>
     </footer>
   );
