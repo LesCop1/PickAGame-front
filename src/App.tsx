b4bcbfa0ea8868581fa6eb8d5ab10ce44722e09f
Mathis Engels <mathisengels@yahoo.fr>
2026-01-21 17:44:23 +0100

    feat(component): add VerdictItem component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import VerdictItem from "./components/VerdictItem/VerdictItem";
+
 export default function App() {
   return (
-    <div>
+    <div className="flex flex-col gap-pag-sizing-25">
       <span>App.tsx</span>
+      <VerdictItem interested={true} src="https://mathisengels.fr/og-image.webp" value="Mathis Engels" />
+      <VerdictItem
+        interested={true}
+        achievementsProgress={75}
+        src="https://mathisengels.fr/og-image.webp"
+        value="Mathis Engels"
+      />
+
+      <VerdictItem interested={false} src="https://mathisengels.fr/og-image.webp" value="Mathis Engels" />
+      <VerdictItem
+        interested={false}
+        achievementsProgress={75}
+        src="https://mathisengels.fr/og-image.webp"
+        value="Mathis Engels"
+      />
     </div>
   );
 }
