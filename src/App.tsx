55aecfc8f6b6a55151fe655ccecd65a1e0a50327
Flavien Carpentier <carpentier.flav@gmail.com>
2026-01-30 22:24:43 +0100

    fix: remove useless utility classes

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import PagLogo from "./assets/logo.png";
 
 export default function App() {
   return (
-    <div className="flex flex-col w-fit h-fit items-center justify-center gap-pag-spacing-300">
-      <img src={PagLogo} alt="PickAGame Logo" className="h-pag-sizing-1300 w-auto" />
-      <span className="text-pag-2xl text-pag-text-primary">Welcome back!</span>
+    <div className="flex flex-col size-fit items-center justify-center gap-pag-spacing-300">
+      <img src={PagLogo} alt="PickAGame Logo" className="h-pag-sizing-1300" />
+      <span className="text-pag-2xl">Welcome back!</span>
       <span className="text-pag-sm text-pag-text-muted">Simplify your gaming sessions now!</span>
       <Button text="Sign in with Steam" logo={SteamLogo} color="bg-pag-bg-secondary" />
       <span className="text-pag-md text-pag-text-muted text-center">
