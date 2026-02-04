9e7cbf2c8c9990b497b8b60833bcc255f54df14a
Flavien Carpentier <carpentier.flav@gmail.com>
2026-02-04 23:23:20 +0100

    update login and logout button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,22 +1,28 @@
 import { Slot } from "@radix-ui/react-slot";
 import { cn } from "../../utils/cn";
+import { cva, type VariantProps } from "class-variance-authority";
+
+const buttonVariants = cva(
+  `
+    flex w-fit items-center gap-pag-spacing-200 rounded-pag-sm p-pag-spacing-200
+    hover:text-pag-text-muted
+  `,
+  {
+    variants: {
+      variant: {
+        default: "bg-pag-bg-secondary text-pag-xl",
+        ghost: "",
+      },
+    },
+  },
+);
 
 export default function Button({
   asChild,
+  variant = "default",
   className,
   ...props
-}: React.ComponentProps<"button"> & { asChild?: boolean }) {
+}: React.ComponentProps<"button"> & VariantProps<typeof buttonVariants> & { asChild?: boolean }) {
   const Comp = asChild ? Slot : "button";
-  return (
-    <Comp
-      className={cn(
-        `
-          flex w-full items-center justify-center gap-pag-spacing-100 space-x-pag-sizing-25 rounded-pag-sm
-          bg-pag-bg-secondary p-pag-spacing-100 text-pag-xl
-        `,
-        className,
-      )}
-      {...props}
-    />
-  );
+  return <Comp className={cn(buttonVariants({ variant }), className)} {...props} />;
 }
