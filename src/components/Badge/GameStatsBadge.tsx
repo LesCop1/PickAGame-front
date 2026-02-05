43e87cd6b97000972b3124d871fe008314876596
Flavien Carpentier <carpentier.flav@gmail.com>
2026-02-05 20:06:30 +0100

    chore(component): make GameStatsBadge more readable

--- src/components/Badge/GameStatsBadge.tsx
+++ src/components/Badge/GameStatsBadge.tsx
@@ -1,31 +1,43 @@
 import { DynamicIcon } from "lucide-react/dynamic";
-import Badge from "./Badge";
-import { UsersRound } from "lucide-react";
 import type { ComponentProps } from "react";
+import Badge from "./Badge";
 
 type Props = {
   value: string | number;
   type: "maxPlayers" | "sessionLength" | "rating" | "achievements";
 };
 
-const statsMapping: Record<Props["type"], [ComponentProps<typeof DynamicIcon>["name"], string]> = {
-  maxPlayers: ["users-round", "Max Players"],
-  sessionLength: ["clock", "Session Length"],
-  rating: ["chart-no-axes-combined","Rating"],
-  achievements: ["award","Achievements"],
-};
 
+const statsMapping: Record<Props["type"], { icon: ComponentProps<typeof DynamicIcon>["name"]; label: string }> = {
+  maxPlayers: {
+    icon: "users-round",
+    label: "Max Players",
+  },
+  sessionLength: {
+    icon: "clock",
+    label: "Session Length",
+  },
+  rating: {
+    icon: "chart-no-axes-combined",
+    label: "Rating",
+  },
+  achievements: {
+    icon: "award",
+    label: "Achievements",
+  }
+};
 
 export default function SessionLengthBadge({ value, type }: Props) {
   return (
     <Badge
       className="
-      flex w-fit items-center gap-pag-sizing-100 rounded-pag-sm bg-pag-bg-primary px-pag-sizing-75 py-pag-sizing-50 border-none
-    "
+        flex w-fit items-center gap-pag-sizing-100 rounded-pag-sm border-none bg-pag-bg-primary px-pag-sizing-75
+        py-pag-sizing-50
+      "
     >
-      <DynamicIcon name={statsMapping[type][0]} size={16} className="text-pag-icon-stat" />
+      <DynamicIcon name={statsMapping[type].icon} size={16} className="text-pag-icon-stat" />
       <div className="flex flex-col text-pag-sm">
-        <span className="text-pag-text-muted">{statsMapping[type][1]}</span>
+        <span className="text-pag-text-muted">{statsMapping[type].label}</span>
         <span className="font-bold">{value}</span>
       </div>
     </Badge>
