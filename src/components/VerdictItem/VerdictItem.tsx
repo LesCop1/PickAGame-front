788e6c894b82a3aeaa39db40289765c15ad91e28
Flavien Carpentier <carpentier.flav@gmail.com>
2026-02-17 22:15:09 +0100

    fix(component): add className to VerdictItem props

--- src/components/VerdictItem/VerdictItem.tsx
+++ src/components/VerdictItem/VerdictItem.tsx
@@ -1,10 +1,12 @@
 import { cva } from "class-variance-authority";
 import Avatar, { type Props as AvatarProps } from "../Avatar/Avatar";
 import { Award } from "lucide-react";
+import { cn } from "../../utils/cn";
 
 interface Props extends AvatarProps {
   interested: boolean;
   achievementsProgress?: number;
+  className?: string;
 }
 
 const verdictItemVariants = cva("flex w-full justify-between rounded-pag-sm border p-pag-spacing-50", {
@@ -16,9 +18,9 @@ const verdictItemVariants = cva("flex w-full justify-between rounded-pag-sm bord
   },
 });
 
-export default function VerdictItem({ interested, achievementsProgress, value, ...avatarProps }: Props) {
+export default function VerdictItem({ interested, achievementsProgress, value, className, ...avatarProps }: Props) {
   return (
-    <div className={verdictItemVariants({ interested })}>
+    <div className={cn(verdictItemVariants({ interested }), className)}>
       <div className="flex items-center gap-pag-spacing-25">
         <Avatar size={"sm"} value={value} {...avatarProps} />
         <span className="text-pag-md text-pag-text-primary">{value}</span>
